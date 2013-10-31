45bad801e318ae9cfa75e6c96703de5c04f38d50
Sergey Panov <gostroy@gmail.com>
2013-11-01 01:08:11 +0200

    modified:   polls/admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from polls.models import Poll, Choice
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
 
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -8,5 +16,5 @@ class PollAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
 
+
 admin.site.register(Poll, PollAdmin)
-admin.site.register(Choice)
