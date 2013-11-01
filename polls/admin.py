41f16ac0588a6ad554345e61dee79d997e7bcb47
Sergey Panov <gostroy@gmail.com>
2013-11-01 22:15:52 +0200

    add method Choice()

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from polls.models import Poll, Choice
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 class PollAdmin(admin.ModelAdmin):
@@ -9,12 +9,8 @@ class PollAdmin(admin.ModelAdmin):
         (None,               {'fields': ['question']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
-
-class PollAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
 
 admin.site.register(Poll, PollAdmin)
