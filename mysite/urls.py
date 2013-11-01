b3f4bc2aa5d09414fe5c5a250c7b7592eb0d9d51
Sergey Panov <gostroy@gmail.com>
2013-11-01 23:19:07 +0200

    modified:   mysite/urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,5 +16,5 @@ urlpatterns = patterns('',
      url(r'^admin/', include(admin.site.urls)),
 
     # for polls
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^polls/', include('polls.urls')),
 )
